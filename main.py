09240b8ead6f9efd9de54e31deb6df6d082de339
a <keyou@Skyo.datalix.de>
2024-03-14 13:11:26 +0700

    Add color to message, update config format, and little bug fix

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os, socket, time, json
 from datetime import datetime
+from colorama import init, Fore, Style, Back
 import requests as rq
 from urllib.parse import urlparse
 from dotenv import load_dotenv as ld
 
+dataMap = {}
+INFO_MESSAGE = Fore.BLUE + Style.BRIGHT + "[Info] " + Style.RESET_ALL
 # Load enviroments
 ld()
 NTFY_SERVER = os.getenv("NTFY_SERVER") or "https://ntfy.sh"
@@ -12,43 +15,75 @@ INTERVAL = os.getenv("INTERVAL") or 120
 
 # Analizing some enviroments
 if not NTFY_TOPIC:
-    print("You must set your ntfy topic in .env file")
+    print(Fore.RED + Style.BRIGHT + "[Error] You must set your ntfy topic in .env file" + Style.RESET_ALL)
     exit(1)
 
 # Analyze whether an Interval is a number or not
 try:
     int(INTERVAL)
 except:
-    print("Interval must be number")
+    print(Fore.RED + Style.BRIGHT + "[Error] Interval must be number" + Style.RESET_ALL)
     exit(1)
 
 def check_NTFY_SERVER():
     try:
         get = rq.get(NTFY_SERVER, timeout=5)
         if not get:
-            print(f"Cannot connect to ntfy server ({NTFY_SERVER})")
+            print(Fore.RED + Style.BRIGHT + f"[Error] Cannot connect to ntfy server ({NTFY_SERVER})" + Style.RESET_ALL)
             exit(1)
     except rq.RequestException:
-        print(f"Cannot connect to ntfy server ({NTFY_SERVER})")
+        print(Fore.RED + Style.BRIGHT + f"[Error] Cannot connect to ntfy server ({NTFY_SERVER})" + Style.RESET_ALL)
         exit(1)
 
-def save_data(data):
-    with open('config.json', 'w') as file:
-        json.dump(data, file, indent=4)
+def save_data(url, status):
+    monitor = {
+        "name": dataMap[url]["name"],
+        "url": url,
+        "status": status
+    }
+    
+    dataMap[url] = monitor
+    
 
-def read_data():
+def read_data(mode):
     try:
-        with open("config.json", "r") as file:   
-            return json.load(file)
+        if mode == 1:
+            urls = []
+            
+            for data in dataMap.values():
+                urls.append(data["url"])
+                
+            return urls
+                
+        elif mode == 2:
+            with open("config.json", "r") as file:   
+                data = json.load(file)
+            
+                urls = []
+                    
+                for name, url in data.items():
+                    urls.append(url)
+                    
+                    monitor = {
+                        "name": name,
+                        "url": url,
+                        "status": None
+                    }
+                        
+                    dataMap[url] = monitor
+                        
+                return urls
 
-    except FileNotFoundError as e:
-        return {}
+    except FileNotFoundError:
+        print(Fore.RED + Style.BRIGHT + "[Error] Cannot get config file" + Style.RESET_ALL)
+        exit(1)
 
-def send_notification(url, status):
-    name = names[urls.index(url)]
+def send_notification(url):
+    name = dataMap[url]["name"]
+    status = dataMap[url]["status"]
     post_url = f"{NTFY_SERVER}/{NTFY_TOPIC}"
     data = f"{name} is {status}\nURL: {url}\nTime: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
-    print(f"{url} is {status}")
+    print(INFO_MESSAGE + f"{url} is {status}")
     response = rq.post(post_url, data=data)
     
 def check_tcp_connection(url):
@@ -60,11 +95,10 @@ def check_tcp_connection(url):
     except (socket.timeout, socket.error) as e:
         return False
 
-def get_request(urls, statuses):
-    print("=" * 30)
+def get_request(urls):
     
     for url in urls:
-        print(f"Checking {url} ...")
+        print(INFO_MESSAGE + f"Checking {url} ...")
         
         index = urls.index(url)
         try:
@@ -75,60 +109,56 @@ def get_request(urls, statuses):
                 get = check_tcp_connection(url)
                 
             else:
-                print(f"Connection type not supported: {url}\n")
+                print(Fore.RED + Style.BRIGHT + f"[Error] Connection type not supported: {url}" + Style.RESET_ALL)
                 continue
 
             # Send Status
             if get:
-                if statuses[index] == "Online":
+                if dataMap[url]["status"] == "Online":
                     continue
                     
-                statuses[index] = "Online"
-                    
-                send_notification(url, statuses[index])
-                save_data({"names": names, "urls": urls, "statuses": statuses})
+                save_data(url, "Online")
+                send_notification(url)
                 
             elif not get:
-                if statuses[index] == "Offline":
+                if dataMap[url]["status"] == "Offline":
                     continue
                 
-                statuses[index] = "Offline"
-                send_notification(url, statuses[index])
-                save_data({"names": names, "urls": urls, "statuses": statuses})
+                save_data(url, "Offline")
+                send_notification(url)
 
                 
         except rq.RequestException:
-            if statuses[index] == "Offline":
+            if dataMap[url]["status"] == "Offline":
                 continue
-
-            statuses[index] = "Offline"
             
-            send_notification(url, statuses[index])
-            save_data({"names": names, "urls": urls, "statuses": statuses})
+            save_data(url, "Offline")
+            send_notification(url)
             
+    print(Fore.YELLOW + Style.BRIGHT + "=" * 40 + Style.RESET_ALL)
     time.sleep(int(INTERVAL))
-    read_data()
-    get_request(urls, statuses)
-
-# Load Data
-monit_data = read_data()
-
-names = monit_data.get('names', [])
-urls = monit_data.get("urls", [])
-statuses = monit_data.get("statuses", [])
+    get_request(read_data(1))
 
 if __name__ == "__main__":
     try:
         check_NTFY_SERVER()
         start_message="Monitoring Device Started"
         rq.post(f"{NTFY_SERVER}/{NTFY_TOPIC}", data=start_message)
-        print(start_message)
-
-        get_request(urls, statuses)
-    except (Exception, KeyboardInterrupt):
+        print(Fore.YELLOW + Style.BRIGHT + "=" * 40 + Style.RESET_ALL)
+        print(INFO_MESSAGE + start_message)
+        print(INFO_MESSAGE + "Press CTRL+C to exit")
+        print(Fore.YELLOW + Style.BRIGHT + "=" * 40 + Style.RESET_ALL)
+        get_request(read_data(2))
+        
+    except KeyboardInterrupt:
         stop_message = "Monitoring Device Stoped"
         rq.post(f"{NTFY_SERVER}/{NTFY_TOPIC}", data=stop_message)
-        print(stop_message)
+        print(INFO_MESSAGE + stop_message)
         exit(1)
+        
+    except Exception as e:
+        print(Fore.RED + Style.BRIGHT + f"[Error] {e}" + Style.RESET_ALL)
+        exit(1)
+        
 
 
